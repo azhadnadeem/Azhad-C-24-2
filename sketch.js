6b27ca4e90079c3cf221bb0021050c4833890c07
azhadnadeem <74958830+azhadnadeem@users.noreply.github.com>
2020-12-29 01:29:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,43 @@
-var ground;
-var ball1;
-var dusbin1,dusbin2,dusbin3;
+
+var dustbin1,dustbin2,dustbin3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+var ground;
+var paper1;
 function preload()
 {
 	
 }
 
 function setup() {
-	createCanvas(1000, 400);
-
+  createCanvas(1000, 400);
+  rectMode(CENTER);
+  engine = Engine.create();
+	world = engine.world;
 groundSprite=createSprite(width/2,380,width,10)
 groundSprite.shapeColor=color("yellow")
 
-dusbin1=createSprite(800,365,150,10)
-dusbin2=createSprite(870,345,10,50)
-dusbin3=createSprite(720,345,10,50)
-dusbin1.shapeColor=color("white")
-dusbin2.shapeColor=color("white")
-dusbin3.shapeColor=color("white")
+dustbin1=createSprite(800,365,150,10)
+dustbin2=createSprite(870,345,10,50)
+dustbin3=createSprite(720,345,10,50)
+dustbin1.shapeColor=color("white")
+dustbin2.shapeColor=color("white")
+dustbin3.shapeColor=color("white")
 
-	engine = Engine.create();
-	world = engine.world;
-ball1=new ball(100,363,25,25)
+	
+paper1=new paper(100,362,25,25)
 	//Create the Bodies Here.
-  ground=Bodies.rectangle(width/2,380,width,10);
+  ground=Bodies.rectangle(width/2,380,width,10,{ bounce:true});
   World.add(world,ground);
 
-  dusbin1=Bodies.rectangle(800,365,150,10,{isStatic:true});
-  World.add(world,dusbin1);
-  dusbin2=Bodies.rectangle(870,345,10,50,{isStatic:true});
-  World.add(world,dusbin2);
-  dusbin3=Bodies.rectangle(720,345,10,50,{isStatic:true});
-  World.add(world,dusbin3);
+  dustbin1=Bodies.rectangle(800,365,150,10,{isStatic:true});
+  World.add(world,dustbin1);
+  dustbin2=Bodies.rectangle(870,345,10,50,{isStatic:true});
+  World.add(world,dustbin2);
+  dustbin3=Bodies.rectangle(720,345,10,50,{isStatic:true});
+  World.add(world,dustbin3);
 
 	Engine.run(engine);
   
@@ -46,15 +47,14 @@ ball1=new ball(100,363,25,25)
 function draw() {
   rectMode(CENTER);
   background(0);
-  ball1.display()
-  
-  drawSprites();
  
+  drawSprites();
+  paper1.display();
 }
 
 
-function keyPressed(){
-  if(keyCode===UP_ARROW){
-    
+function KeyPressed(){
+  if(keyCode === UP_ARROW){
+   Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85}); 
   }
 }
